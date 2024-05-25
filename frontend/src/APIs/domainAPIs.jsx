4aaf32f590b195c680a1a2f3a38b0ed13e3d433a
Nithin Kumar Appari <52022957+nithin3q@users.noreply.github.com>
2024-05-25 19:04:04 +0530

    Update domainAPIs.jsx

--- frontend/src/APIs/domainAPIs.jsx
+++ frontend/src/APIs/domainAPIs.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const DOMAINAPIs = axios.create({
-  baseURL: "http://localhost:8080/api/domain",
+  baseURL: "https://dns-manager-khaki.vercel.app/api/domain",
 });
 
 export async function listDomains() {
